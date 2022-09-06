a4f568eba81b38c96ad3ff3accb8b267934d2538
Chihiro890 <chihiro262001@yahoo.co.jp>
2022-09-07 07:22:23 +0900

    制御構文1の課題

---
+++ 04/05_if.php
@@ -0,0 +1,25 @@
+<?php
+
+echo '$numの値を入力してください: ';
+$num = trim(fgets(STDIN));
+
+// ここに処理を記述
+$judge = True;
+
+if ($num == 1) {
+    $judge = False;
+}
+
+for ($i = 2; $i < $num; $i++) {
+    if ($num % $i == 0) {
+        $judge = False;
+  // 素数と判定されたら処理を抜ける
+        break;
+    }
+}
+
+if ($judge) {
+    echo $num . 'は素数です。';
+} else {
+    echo $num . 'は素数ではありません。';
+}
